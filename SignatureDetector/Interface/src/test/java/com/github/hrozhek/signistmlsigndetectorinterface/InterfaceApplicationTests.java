2325f8344b8eca26f014f6a153cbd192226158a9
hrozh <hrozhek@gmail.com>
2023-05-22 21:05:23 +0300

    Project structure added for ML libs

---
+++ SignatureDetector/Interface/src/test/java/com/github/hrozhek/signistmlsigndetectorinterface/InterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hrozhek.signistmlsigndetectorinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
