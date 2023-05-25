4a6c04fabd74f25fa16db805fcdc62e5eda0a2e0
hrozh <hrozhek@gmail.com>
2023-05-25 11:29:32 +0300

    swagger is alive for the only application, check http://localhost:7070/swagger-ui/index.html

--- SignaturistApplication/Application/src/main/java/com/github/hrozhek/signistappsignaturist/controller/ModelController.java
+++ SignaturistApplication/Application/src/main/java/com/github/hrozhek/signistappsignaturist/controller/ModelController.java
@@ -2,27 +2,27 @@ package com.github.hrozhek.signistappsignaturist.controller;
 
 import com.github.hrozhek.signistappsignaturist.dto.ModelResponse;
 import com.github.hrozhek.signistappsignaturist.dto.RegisterModelRequest;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 
 //todo id сессии, и из нее берем
 public interface ModelController {
 
-//    @ApiResponse(responseCode = "200", description = "ok")
-//    @ApiResponse(responseCode = "400", description = "hmm")//todo
+    @ApiResponse(responseCode = "200", description = "ok")
+    @ApiResponse(responseCode = "400", description = "hmm")//todo
     ResponseEntity<ModelResponse> getModel(long personId); //todo check PersonService
 
-//    @ApiResponse(responseCode = "200", description = "ok")
-//    @ApiResponse(responseCode = "400", description = "hmm")//todo
+    @ApiResponse(responseCode = "200", description = "ok")
+    @ApiResponse(responseCode = "400", description = "hmm")//todo
     ResponseEntity<ModelResponse> addModel(long personId); //todo personId matters
 
 //    /*void*/ Object enrichModel(long personId, Object image); //todo only personId matters
 
-//    @ApiResponse(responseCode = "200", description = "ok")
-//    @ApiResponse(responseCode = "400", description = "hmm")//todo
+    @ApiResponse(responseCode = "200", description = "ok")
+    @ApiResponse(responseCode = "400", description = "hmm")//todo
     ResponseEntity deleteModel(long id);
 
-//    @ApiResponse(responseCode = "200", description = "ok")
-//    @ApiResponse(responseCode = "400", description = "hmm")//todo
+    @ApiResponse(responseCode = "200", description = "ok")
+    @ApiResponse(responseCode = "400", description = "hmm")//todo
     ResponseEntity<Long> registerModel(RegisterModelRequest request);
 }
--- SignaturistApplication/Application/src/main/java/com/github/hrozhek/signistappsignaturist/controller/VerificationController.java
+++ SignaturistApplication/Application/src/main/java/com/github/hrozhek/signistappsignaturist/controller/VerificationController.java
@@ -2,14 +2,14 @@ package com.github.hrozhek.signistappsignaturist.controller;
 
 import com.github.hrozhek.signistappsignaturist.dto.VerificationRequest;
 import org.springframework.http.ResponseEntity;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 public interface VerificationController {
 
 //    List<Object> getHistory(long personId); //todo e.g. userId timestamp result - success/failure (in case of exceptions) and score if succeeded etc.
 
-//    @ApiResponse(responseCode = "200", description = "ok")
-//    @ApiResponse(responseCode = "400", description = "hmm")//todo
+    @ApiResponse(responseCode = "200", description = "ok")
+    @ApiResponse(responseCode = "400", description = "hmm")//todo
     ResponseEntity<Boolean> verify(VerificationRequest signature); // todo - должен остаться в истории и быть доступен результат сравнения
 
     //todo нужно ли хранить переданные образцы? Как будет происходить enrich модели
